335c7fc554b21e301e80bc5589ede85f11f5f4d3
asus <natakulibaba88@gmail.com>
2022-04-02 01:03:49 +0300

    rewrite modal by hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,36 @@
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import s from '../Modal/Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+function Modal({ onClose, children }) {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose(e);
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [onClose]);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleBackdropclick = e => {
+  const handleBackdropclick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={s.overlay} onClick={this.handleBackdropclick}>
-        <div className={s.modal}>{this.props.children}</div>
-        <button
-          className={s.btnCloseModal}
-          type="button"
-          onClick={this.props.onClose}
-        >
-          x
-        </button>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={s.overlay} onClick={handleBackdropclick}>
+      <div className={s.modal}>{children}</div>
+      <button className={s.btnCloseModal} type="button" onClick={onClose}>
+        x
+      </button>
+    </div>,
+    modalRoot
+  );
 }
+
 export default Modal;
